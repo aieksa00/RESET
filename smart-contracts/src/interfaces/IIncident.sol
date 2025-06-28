3fdf901116f9d29217b52e5adbe6250c78293579
UrosZigic <uros.zigic@gmail.com>
2025-06-29 00:06:49 +0200

    add getters

--- smart-contracts/src/interfaces/IIncident.sol
+++ smart-contracts/src/interfaces/IIncident.sol
@@ -7,4 +7,6 @@ interface IIncident {
     function acceptOffer(uint256 _offerId) external;
 
     function RejectOffer(uint256 _offerId) external;
+
+    function getHackerAddress() external view returns (address);
 }
